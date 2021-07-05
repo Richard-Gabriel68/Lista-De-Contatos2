090a28b2e94e2b7631835648fdc2845644478aaf
Richard Gabriel <rickanimals02@gmail.com>
2021-07-05 19:04:20 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+        lista: [
+            {nome: 'João', tel: 23543455},
+            {nome: 'Maria', tel: 73283335},
+            {nome: 'Marta', tel: 21245644}
+        ],
+        pesquisa: '',
+    },
+    computed: {
+        
+    },
+})
