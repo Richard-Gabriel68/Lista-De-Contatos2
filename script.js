e30cb0772136103422662c4a483ae123f7107699
Richard Gabriel <rickanimals02@gmail.com>
2021-07-06 15:19:18 -0300

    Estilização e criação das seções

--- script.js
+++ script.js
@@ -7,8 +7,54 @@ let app = new Vue ({
             {nome: 'Marta', tel: 21245644}
         ],
         pesquisa: '',
+        nome: '',
+        tel: '',
+
+        adicionar : true,
+        pesquisar: false,
+        mostrar: false
+    },
+    methods: {
+        create: function(){
+            this.adicionar = true;
+            this.pesquisar = false;
+            this.mostrar = false;
+        },
+        add: function(){
+            let newContact = {nome: this.nome, tel: this.tel};
+            
+            this.lista.push(newContact);
+            this.nome = '';
+            this.tel = '';
+        },
+        goBack: function(){
+            this.adicionar = false;
+            this.pesquisar = true;
+            this.mostrar = false;
+        },
+        show: function(){
+            this.adicionar = false;
+            this.pesquisar = false;
+            this.mostrar = true;
+        }
     },
     computed: {
-        
-    },
+        listaF: function (){
+            return this.lista.filter(function(item){
+                if (this.pesquisa != ''){
+                    
+                    if (item.nome.indexOf(this.pesquisa) > -1) {
+                        return true;
+                    }
+
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }, this)
+        }
+    }
 })
